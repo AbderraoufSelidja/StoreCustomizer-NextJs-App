5dcfb8856ec11735c7c895a3d9e13cb68d460dc2
Abderraouf Selidja <104903305+AbderraoufSelidja@users.noreply.github.com>
2024-10-10 10:57:18 +0100

    Update page.js

--- app/stores/[storeId]/page.js
+++ app/stores/[storeId]/page.js
@@ -1,46 +1,26 @@
 // StorePage.jsx or StorePage.tsx
-"use client";
-
-import React, { useEffect, useState } from "react";
-import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { app } from "@/app/components/Firebase";
 import StoreContent from "./StoreContent"; // Import your client component
-import Swal from "sweetalert2";
 
-const StorePage = ({ params }) => {
-  const [store, setStore] = useState(null);
+export async function generateStaticParams() {
   const db = getFirestore(app);
+  const storeIds = [];
 
-  useEffect(() => {
-    const fetchStoreContent = async () => {
-      const storeId = params.storeId; // Get storeId from params
-
-      try {
-        const docRef = doc(db, "stores", storeId);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          setStore(docSnap.data());
-        } else {
-          console.error("No such document!");
-        }
-      } catch (error) {
-        console.error("There was an error fetching the store content!", error);
-      }
-    };
-
-    fetchStoreContent();
-  }, [params.storeId]);
-
-  if (!store) {
-    return (
-      <h2>
-        <strong>Loading...</strong>
-      </h2>
-    );
+  try {
+    const querySnapshot = await getDocs(collection(db, "stores"));
+    querySnapshot.forEach((doc) => {
+      storeIds.push(doc.id); // Push each store ID into the array
+    });
+  } catch (error) {
+    console.error("Error fetching store IDs: ", error);
   }
 
-  return <StoreContent store={store} />;
+  return storeIds.map((id) => ({ storeId: id }));
+}
+
+const StorePage = async ({ params }) => {
+  return <StoreContent params={params} />;
 };
 
 export default StorePage;
